ef7107c8692356b5c9daa215422004bd3913d85c
Nami5011 <maron.011821@gmail.com>
2020-10-08 23:36:18 +0900

    added header

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="サンプルマーケティングの公式企業サイトです。サンプルマーケティングの事業内容、会社概要、個人情報保護の取り組みなどを掲載しています。">
+  <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <!-- ページ全体の囲み枠 -->
+  <div id="wrapper">
+
+    <!-- ロゴマーク -->
+    <header>
+      <h1>サンプルマーケティング | 海外輸入とインターネット通販でお客様に幸せをお届けします。</h1>
+      <div id="header-logo">
+        <a href="index.html"><img src="img/header_logo.png" alt="サンプルマーケティング株式会社"></a>
+      </div>
+    </header>
+
+    <!-- グローバルナビゲーション -->
+    <nav id="gNav">
+      <ul>
+        <li class="current-menu-item"><a href="index.html">ホーム</a></li>
+        <li><a href="business.html">事業内容</a></li>
+        <li><a href="about.html">会社概要</a></li>
+        <li><a href="contact.html">お問い合わせ</a></li>
+      </ul>
+    </nav>
