7b67b04e24f3c2d85c931ef742d6f0209a6bc063
Nami5011 <maron.011821@gmail.com>
2020-10-15 01:21:05 +0900

    created footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+<!-- フッターエリア -->
+		<footer>
+			<?php
+			$param = array(
+				'container' => 'nav',
+				'container_id' => 'fNav',
+				'theme_location' => 'footer_nav', // @todo function.phpで定義したfooter_navと連動
+			);
+			wp_nav_menu( $param );
+			?>
+			<small id="copyright">Copyright (c) 2016 Sample Marketing.inc. All Rights Reserved.</small>
+		</footer>
+		</div>
+		<?php wp_footer(); ?>
+		</body>
+
+		</html>
