5420207c64e508a1ae05c9eacd81dea35267698b
Nami5011 <maron.011821@gmail.com>
2020-10-14 18:47:54 +0900

    changed header

--- functions.php
+++ functions.php
@@ -1,18 +1,23 @@
 <?php 
-function the_slug(){
-	$slug= '';
-	// カテゴリーアーカイブページが表示されている場合
-	if (is_category()) {
-		$category= get_the_category(); // カテゴリーの情報を取得
-		$slug= $category[0] -> category_nicename; // カテゴリーのスラッグを取得
+function the_slug() {
+	$slug = '';
+	// @todo カテゴリーアーカイブページが表示されている場合
+	if ( is_category() ) {
+		$category = get_the_category(); // @todo カテゴリーの情報を取得
+		$slug = $category[0]->category_nicename; // @todo カテゴリーのスラッグを取得
 	}
-	// それ以外のページが表示されている場合
+	// @todo それ以外のページが表示されている場合
 	else {
-		$id= get_the_ID(); // ページのIDを取得
-		$page= get_post($id); // IDに該当するページの情報を取得
-		$slug= $page -> post_name; // ページのスラッグを取得
+		$id = get_the_ID(); // @todo ページのIDを取得
+		$page = get_post( $id ); // @todo IDに該当するページの情報を取得
+		$slug = $page->post_name; // @todo ページのスラッグを取得
 	}
-	// スラッグを出力する
+	// @todo スラッグを出力する
 	echo $slug;
 }
+$params = array(
+	'global_nav' => 'グローバルナビゲーション',
+	'footer_nav' => 'フッターナビゲーション',
+);
+register_nav_menus( $params );
 ?>
--- header.php
+++ header.php
@@ -5,11 +5,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
 	<meta name="description" content="サンプルマーケティングの公式企業サイトです。サンプルマーケティングの事業内容、会社概要、個人情報保護の取り組みなどを掲載しています。">
-	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>\favicon.ico">
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>\apple-touch-icon.png">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>\reset.css">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+
+	<img src="<?php echo esc_url( get_template_directory_uri() . '/img/logo.svg' ); ?>" alt="">
+
+	<link rel="shortcut icon" href="<?php echo esc_url( get_stylesheet_directory_uri( '/wp-content/themes/sample_marketing' ) ); ?>/favicon.ico">
+	<link rel="apple-touch-icon" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/wp-content/themes/sample_marketing' ); ?>/apple-touch-icon.png">
+	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/wp-content/themes/sample_marketing' ); ?>/reset.css">
+	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri( '/wp-content/themes/sample_marketing' ) ); ?>">
 	<?php wp_head(); ?>
 </head>
 
@@ -22,11 +25,20 @@
 		<header>
 			<h1>サンプルマーケティング | 海外輸入とインターネット通販でお客様に幸せをお届けします。</h1>
 			<div id="header-logo">
-				<a href="index.html"><img src="img/header_logo.png" alt="サンプルマーケティング株式会社"></a>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_stylesheet_directory_uri( '/wp-content/themes/sample_marketing' ) ); ?>/header_logo.png" alt="サンプルマーケティング株式会社"></a>
 			</div>
 		</header>
 
 		<!-- グローバルナビゲーション -->
+		<?php
+		$param = array(
+			'container' => 'nav',
+			'container_id' => 'gNav',
+			'theme_location' => 'global_nav',
+		);
+		wp_nav_menu( $param );
+		?>
+		<!-- 
 		<nav id="gNav">
 			<ul>
 				<li class="current-menu-item"><a href="index.html">ホーム</a></li>
@@ -34,4 +46,4 @@
 				<li><a href="about.html">会社概要</a></li>
 				<li><a href="contact.html">お問い合わせ</a></li>
 			</ul>
-		</nav>
+		</nav> -->
