339745c44a9a8ba057fd212fd57799778e134cbc
Nami5011 <maron.011821@gmail.com>
2020-10-18 16:52:39 +0900

    added single page

---
+++ single.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MyClass Class Doc Comment
+ *
+ * @category Page
+ * @package  MyPackage
+ * @author    Nami Ueda
+ */
+
+?>
+<?php get_header(); ?>
+		<!-- コンテンツエリア -->
+		<section id="contents">
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					?>
+			<!--記事-->
+			<article>
+				<!--ページのタイトル-->
+				<h2><?php the_title(); ?></h2>
+				<div class="inner">
+					<!--メタ情報-->
+					<div class="entry-meta">
+						<span class="date"><?php the_time( 'Y/m/d' ); ?></span>
+						<span class="cate"><?php the_category( ', ' ); ?></span>
+					</div>
+					<!--ページの本文-->
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+					<?php edit_post_link( '編集', '<p class="post-edit-link">', '</p>' ); ?>
+				</div>
+			</article>
+			<!--//記事-->
+					<?php
+				endwhile;
+			endif;
+			?>
+		</section>
+		<!--//コンテンツエリア-->
+		<?php
+		get_footer();
+		?>
